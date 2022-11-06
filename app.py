c544626945db74010fb9f86cdd79458dbafeda25
Hossein Taheri <shhtaheri@gmail.com>
2022-11-06 09:47:24 +0330

    Add Second Solution

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ import re
 def fetch_domain_name(url):
     return re.sub(".+\/\/|www.|\..+", "", url)
 
+# Second Solution
+def domain_name(url):
+    return url.split("//")[-1].split("www.")[-1].split(".")[0]
+    
 dn = fetch_domain_name("https://github.com")
 print(dn)
